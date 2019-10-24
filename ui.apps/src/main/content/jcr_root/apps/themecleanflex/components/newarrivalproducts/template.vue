26839632c3aefcb1144859a29192efed4f9830e0
Swapnil M Mane <swapnil.mane@hotwaxsystems.com>
2019-10-24 13:05:30 +0530

    Implemented: 'New Arrival Products' component (CMS-100011)
    This component will be used to render the new arrival products.
    It has two properties:
    Title - Title to be rendered for this block
    URL - The URL of instance through which new arrival products will be fetched.

---
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/newarrivalproducts/template.vue
@@ -0,0 +1,15 @@
+<template>
+  <themecleanflex-components-block v-bind:model="model">
+    <div>
+      <h3>{{model.title}}</h3>
+      <p>New Arrival Products will be shown here, please preview the Storefront.</p>
+    </div>
+  </themecleanflex-components-block>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
