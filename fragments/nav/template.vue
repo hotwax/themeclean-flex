d806409ad9f458417bef4bfbcbfbd9bec6fb033c
Saurabh Tiwari <saurabh.tiwari@hotwax.co>
2020-01-03 15:14:40 +0530

    Added nav component on thememeclean-flex.(1vr4jb)

---
+++ fragments/nav/template.vue
@@ -0,0 +1,21 @@
+<template>
+  <themecleanflex-components-block v-bind:model="model">
+    <div class="w-full">
+      <div>
+        <h2 v-html="model.title"></h2>
+        <div class="menu" v-for="(item,i) in model.nav_item" :key="i">
+          <a v-bind:href="item.url">
+            <p v-html="item.title"></p>
+          </a>
+        </div>
+      </div>
+    </div>
+  </themecleanflex-components-block>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
