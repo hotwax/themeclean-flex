7eab4d17e21d8eca4ca8964e21d079d0a3099c67
Saurabh Tiwari <saurabh.tiwari@hotwax.co>
2020-01-06 12:24:00 +0530

    Improved: Added copyright component on themeclean-flex (1vrakc)

---
+++ fragments/copyright/template.vue
@@ -0,0 +1,14 @@
+<template>
+  <themecleanflex-components-block v-bind:model="model">
+    <div>
+      <p v-html="model.text"></p>
+    </div>
+  </themecleanflex-components-block>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
