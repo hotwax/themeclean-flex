5f75b68d117a5c03bc3c687e17d67b0fd17e75f4
Saurabh Tiwari <saurabhhtiwarii@gmail.com>
2020-02-25 18:03:58 +0530

    Implemented: Added appstore component on themecleanflex(#1zm1v3)

---
+++ fragments/appstore/template.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main" v-bind:data-per-path="model.path">
+    <div class="hero-image">
+      <div class="hero-text">
+        <h2 style="font-size:50px"></h2>
+        <p></p>
+        <button></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
