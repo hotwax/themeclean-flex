c92179f12adee3d7adb502d11a2fb94f8347de3b
Saurabh Tiwari <saurabhhtiwarii@gmail.com>
2020-04-21 19:18:35 +0530

    Improved: Improved Navigation component UI on themecleanflex (#2qj1mt)

--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/nav/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/nav/template.vue
@@ -1,12 +1,12 @@
 <template>
   <div :class = "'nav-menu-item-level-'+navItemLevel" v-bind:data-per-path="model.path">
-    <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'before' }">
+    <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.title, location: 'before' }">
     </pagerender-vue-components-placeholder>
     <a v-html="model.title"></a>
     <template v-for="child in model.children">
       <component v-bind:is="child.component" v-bind:model="child"></component>
     </template>
-    <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.component, location: 'after' }">
+    <pagerender-vue-components-placeholder v-bind:model="{ path: model.path, component: model.title, location: 'after' }">
     </pagerender-vue-components-placeholder>
   </div>
 </template>
